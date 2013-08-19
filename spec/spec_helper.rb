4b0d1cca3d6c8d068f8c72017bb15b6a6851c2ca
Dan Reed <dan@danreed.net>
2013-08-19 11:43:25 -0700

    dummy rails project, rspec working again

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,12 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
 require 'cloud_front_helpers'
+
+Rails.backtrace_cleaner.remove_silencers!
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
